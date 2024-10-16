c62934b2beb9cc7315b2c28305a6e1c51759342d
Ezequiel Muñoz <166835002+EzekielMV@users.noreply.github.com>
2024-10-16 14:50:13 -0300

    Ejercicio 2,3 - Guía 6

--- Python/guia-6.py
+++ Python/guia-6.py
@@ -1,26 +1,21 @@
 import math
 # Ejercicio 1
-# 1
 def imprimir_hola_mundo():
     mensaje = "Hola mundo"
     print(mensaje)
 
-# 2
 def imprimir_un_verso():
     mensaje = "Hola, ¿Cómo estas?.\nSe que estas bien. \nMe alegro :)" 
     return mensaje
 
-# 3
 def raizDe2(num:int):
     res = round(math.sqrt(num), 4)
     return res
 
-# 4
 def factorial_de_dos():
     res = math.factorial(2)
     return res
 
-# 5
 def perimetro(): 
     res = round(2*math.pi,5)
     return res
@@ -43,14 +38,26 @@ def imprimir_dos_veces(estribillo:str)->str:
     return res
 
 def es_multiplo_de(n:int, m:int)->bool:
-    k:int = 0
-    n == m * k = True
-    print()
-    
-    
-    
-    
-    
-    
-    
-print(imprimir_dos_veces("abc\ndef\nghi\n"))
+    resto_n_m = n % m
+    return resto_n_m == 0
+
+def es_par(n:int) -> bool:
+    return es_multiplo_de(n,2)
+
+def cantidad_de_pizzas(comensales:int, min_cant_de_porciones:int):
+    return round((comensales*min_cant_de_porciones)/8)
+
+# Ejercicio 3
+def alguno_es_0(numero1:float,numero2:float) -> bool:
+    return numero1 or numero2 == 0
+
+def ambos_son_0(numero1:float, numero2:float) -> bool:
+    return numero1 and numero2 == 0
+
+def es_nombre_largo(nombre:str) -> bool:
+    return 3 <= len(nombre) <= 8
+
+def es_bisiesto(year):
+    return es_multiplo_de(year,400) or es_multiplo_de(year,4) and not es_multiplo_de(year,100)
+
+# Ejercicio 4
