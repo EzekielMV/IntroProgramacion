c5f74be74c2ac276e066be10643071c3a0aba4fe
Ezequiel Muñoz <166835002+EzekielMV@users.noreply.github.com>
2024-10-21 21:46:20 -0300

    Update guia-6.py

--- Python/guia-6.py
+++ Python/guia-6.py
@@ -61,3 +61,106 @@ def es_bisiesto(year):
     return es_multiplo_de(year,400) or es_multiplo_de(year,4) and not es_multiplo_de(year,100)
 
 # Ejercicio 4
+def peso_pino(metros:int)->int:
+    altura = metros*100
+    if altura <= 3 * 100:
+        resultado = altura * 3
+    else:
+        resultado = 300 * 3 + (altura - 300) * 2 
+    return resultado
+
+def es_peso_util(peso):
+    peso_max:int = 1000
+    peso_min:int = 400
+    return peso_min <= peso <= peso_max
+
+def sirve_pino(altura):
+    peso_max:int = 100
+    peso_min:int = 400
+    return peso_min <= peso_pino(altura) <= peso_max
+
+def sirve_pino2(altura):
+    return es_peso_util(peso_pino(altura))
+
+# Ejercicio 5 -- Utilizanod las condicionales if/else
+def devolver_el_doble_si_es_par(numero):
+    if numero % 2 == 0:
+        res = numero * 2
+    else:
+        res = numero
+    return res
+
+def devolver_valor_si_es_par_sino_el_que_sigue(numero):
+    if numero % 2 == 0:
+        res = numero
+    else: 
+        res = numero + 1
+    return res
+
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(numero): 
+    if numero % 3 == 0: 
+        res = numero * 2
+    elif numero % 9 == 0:
+        res = numero * 3
+    else: 
+        res = numero
+    return res
+
+def lindo_nombre(nombre):
+    if len(nombre) >= 5:
+        print("Tu nombre tiene muchas letras")
+    else:
+        print("Tu nombre tiene menos de 5 caracteres")
+
+def elRango(numero):
+    if numero < 5:
+        print("Menor a 5")
+    elif 10 <= numero <= 20:
+        print("Entre 10 y 20")
+    elif numero > 20:
+        print("Mayor a 20")
+
+def jubilarse(edad:int, sexo:str):
+    vacaciones = "Ve de vacaciones"
+    if edad < 18 and sexo == "M" or sexo == "F":
+        print(vacaciones)
+    elif sexo == "F" and edad > 60:
+        print(vacaciones)
+    elif sexo == "M" and edad > 65:
+        print(vacaciones)
+    else:
+        print("Ve a trabajar")
+
+# Ejercicio 6
+def unoal10():
+    i:int = 0
+    while i < 10:
+        i += 1
+        print(i)
+
+def diez_a_cuarenta():
+    i:int = 10
+    while i < 40:
+        i += 2
+        print(i)
+
+def eco_diezveces():
+    i:int = 0
+    while i < 10:
+        print("Eco")
+        i += 1
+
+def cuenta_regresiba():
+    i:int = 11
+    while i > 1:
+        i -= 1
+        print(i)
+    print("Despegue")
+
+def viaje_en_el_tiempo(año_de_llegada:int, año_de_partida:int):
+    while año_de_partida < año_de_llegada :
+        año_de_llegada -= 1
+        print(f'Viajo un año al pasado, estamos en el año {año_de_llegada}')
+    print(f"Llegamos al año que querias. ¿Estas feliz?")
+
+viaje_en_el_tiempo(2024,2017)
